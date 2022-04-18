78acecf245e52ac0dd579db71a2e49177d52f0a8
BatcheDaka <dakata@gmail.com>
2022-04-18 22:41:42 +0300

    authentication

--- src/app/modules/authen/app.module.ts
+++ src/app/modules/authen/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { MaterialModule } from 'src/app/material-ui.module';
 import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
     declarations: [
-    LoginComponent
+      LoginComponent
   ],
     imports: [
-  
+  MaterialModule
     ],
     providers: [],
     bootstrap: []
--- src/app/modules/authen/components/login/login.component.ts
+++ src/app/modules/authen/components/login/login.component.ts
@@ -1,15 +1,32 @@
+// import { Component, OnInit } from '@angular/core';
+// import {LoginService } from '../../services/login.service';
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+// export class LoginComponent implements OnInit {
+
+//   constructor(private login:LoginService) { }
+
+//   ngOnInit(): void {
+//   }
+//   onLogin() {
+// this.login.loginWithGoogle();
+//   }
+
+// }
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(public authService: AuthService) {}
+  ngOnInit() {}
+  
 }
